55ac788783507e97cb4b1ce25dec3e5fcb4b9632
Gifar Al Anshar <gifar.anshar12@gmail.com>
2022-06-13 18:39:59 +0700

    add email_verified_at migration

---
+++ db/migrate/20220613113824_create_email_verified_at.rb
@@ -0,0 +1,8 @@
+class CreateEmailVerifiedAt < ActiveRecord::Migration[7.0]
+  def change
+    create_table :email_verified_ats do |t|
+      t.integer :user_id
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_13_112548) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_13_113824) do
+  create_table "email_verified_ats", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.integer "role_id"
     t.string "profile"
