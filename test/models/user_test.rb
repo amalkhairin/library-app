510dd89d623714d808f956c7bffcb1f597795539
Gifar Al Anshar <gifar.anshar12@gmail.com>
2022-06-13 18:48:48 +0700

    add validation for user, name should be present

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,10 +2,15 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   setup do 
-    @user = User.new()
+    @user = User.new(name: "Gifar")
   end
 
   test "User should be valid" do
     assert @user.valid?
+  end
+
+  test "name should be presence" do 
+    @user.name = ""
+    assert_not @user.valid?
   end
 end
