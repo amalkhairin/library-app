f6121a3f1cf820eb7511673dd46622c42517e67a
Gifar Al Anshar <gifar.anshar12@gmail.com>
2022-06-13 21:52:24 +0700

    add one-to-one association between users and email_verified_ats

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_one :email_verified_at
   NAME_FORMAT = /\A(?:[-a-z']+|[-a-z']+\s[-a-z']*\s?[-a-z']+)\z/i 
   validates :name, presence: true, length: {maximum: 255}, format: {with: NAME_FORMAT} 
 
@@ -7,7 +8,7 @@ class User < ApplicationRecord
 
   validates :username, presence: true, length: {maximum: 20, minimum: 3}, uniqueness: true
 
-  PHONE_FORMAT = /\A\d+\Z/
+  PHONE_FORMAT = /\A\d+\Z/ 
   validates :telephone, presence: true, format: {with: PHONE_FORMAT}
 
   validates :password, presence: true, length: {minimum: 5}
