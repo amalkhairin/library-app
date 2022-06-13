b4dee8108adff6c03c7c74e947f16fa2986120a9
Gifar Al Anshar <gifar.anshar12@gmail.com>
2022-06-13 18:43:53 +0700

    add test for User, User nmodel should be valid

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do 
+    @user = User.new()
+  end
+
+  test "User should be valid" do
+    assert @user.valid?
+  end
 end
