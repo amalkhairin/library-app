0a5daef68b4e3a6aa48c9ffe5330ab6edaaccb55
Gifar Al Anshar <gifar.anshar12@gmail.com>
2022-06-14 16:24:46 +0700

    create test for users_controller.rb, should get index

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show edit update destroy ]
+
+  def index
+    @users = User.all
+
+    render json: @users
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  private 
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :username, :password, :telephone)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users, only: %i[ index show edit update new create destroy ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
