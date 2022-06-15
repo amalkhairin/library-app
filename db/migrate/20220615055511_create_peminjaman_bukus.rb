8ea70089530f01b348385326a5a859044128e6d9
imadeekosatria <imadeekosatriawiguna@gmail.com>
2022-06-15 22:00:17 +0800

    temporarily remove reference PeminjamanBuku to User for testing purpose

--- db/migrate/20220615055511_create_peminjaman_bukus.rb
+++ db/migrate/20220615055511_create_peminjaman_bukus.rb
@@ -1,7 +1,7 @@
 class CreatePeminjamanBukus < ActiveRecord::Migration[7.0]
   def change
     create_table :peminjaman_bukus do |t|
-      t.integer :id_user
+      # t.integer :id_user
       t.integer :id_buku
       t.datetime :jadwal_pinjam
       t.datetime :jadwal_kembali
