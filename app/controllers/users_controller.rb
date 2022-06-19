817db06f4ac4917e6039e998d8556392a95a31dd
Gifar Al Anshar <gifar.anshar12@gmail.com>
2022-06-19 16:30:49 +0700

    create login session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_action :authenticate_request, only: %i[ create index ]
   before_action :set_user, only: %i[ show update destroy ]
 
   def index
@@ -22,7 +23,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user
+      render json: @user, statu: :ok
     else
       render json: @user.errors, status: :unprocessable_entity
     end
