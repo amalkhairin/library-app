da772a44ab8661d2ef78a2ce2eb068cc61b3de12
Gifar Al Anshar <gifar.anshar12@gmail.com>
2022-06-23 16:59:28 +0700

    add controller test for buku, 'should not create a new book if not admin'

--- test/controllers/buku_controller_test.rb
+++ test/controllers/buku_controller_test.rb
@@ -8,6 +8,9 @@ class BukuControllerTest < ActionDispatch::IntegrationTest
     @role = Role.create(role: 'admin')
     @user = User.create(name: 'Gifar', email: 'halo@example.com', username: 'gifaraja', telephone: '0812345678910',
                         password: 'admin1', role_id: 1)
+    @role = Role.create(role: 'visitor')                 
+    @user2 = User.create(name: 'Gifar Kedua', email: 'halo2@example.com', username: 'gifaraja2', 
+                        telephone: '0812345678911',password: 'admin1', role_id: 2)
   end
 
   test "should list all books" do 
@@ -24,6 +27,17 @@ class BukuControllerTest < ActionDispatch::IntegrationTest
     auth_token = sign_in_as(@user)
 
     assert_difference('Buku.count', 1) do 
+      post '/buku',
+           params: { buku: { category_id: 1, barcode: '142352525', isbn: '25252525-23', judul: 'Hai Dunia', penulis: 'Fulan', jumlah_buku: 1, is_available: true } }, headers: {HTTP_AUTHORIZATION: "JWT #{auth_token}"}
+    end
+
+    assert_response :success
+  end
+
+  test 'should not create a new book if not admin' do 
+    auth_token = sign_in_as(@user2)
+
+    assert_no_difference('Buku.count') do 
       post '/buku',
            params: { buku: { category_id: 1, barcode: '142352525', isbn: '25252525-23', judul: 'Hai Dunia', penulis: 'Fulan', jumlah_buku: 1, is_available: true } }, headers: {HTTP_AUTHORIZATION: "JWT #{auth_token}"}
     end
