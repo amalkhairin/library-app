3d3c67acd4a00e1a9d7b4fa05103ab2baa8f0639
Gifar Al Anshar <gifar.anshar12@gmail.com>
2022-06-25 17:01:05 +0700

    clean categories_controller_test.rb code

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -5,10 +5,14 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     @category = categories(:category1)
     role = Role.create(role: 'admin')
     role = Role.create(role: 'visitor')
+
     @user = User.create(name: 'Gifar', email: 'halo@example.com', username: 'gifaraja', telephone: '0812345678910',
                         password: 'admin1', role_id: 1)
     @user2 = User.create(name: 'Gifar Dua', email: 'halo2@example.com', username: 'gifaraja2',
                         telephone:'0812345678910',password: 'admin1', role_id: 2)
+
+    @admin_token = sign_in_as(@user)
+    @user_token = sign_in_as(@user2)
   end
 
   test "should list all categories" do
@@ -17,20 +21,16 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "create category if admin" do
-    auth_token = sign_in_as(@user)
-
     assert_difference("Category.count", 1) do
-      post categories_url, params: { category: { name: 'IPA' } }, headers: {HTTP_AUTHORIZATION: "JWT #{auth_token}"}
+      post categories_url, params: { category: { name: 'IPA' } }, headers: {HTTP_AUTHORIZATION: "JWT #{@admin_token}"}
     end
 
     assert_response :created
   end
 
   test "should not create category if not admin" do
-    auth_token = sign_in_as(@user2)
-
     assert_no_difference("Category.count") do
-      post categories_url, params: { category: { name: "Math" } }, headers: {HTTP_AUTHORIZATION: "JWT #{auth_token}"}
+      post categories_url, params: { category: { name: "Math" } }, headers: {HTTP_AUTHORIZATION: "JWT #{@user_token}"}
     end
 
     assert_response :success
@@ -42,37 +42,31 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update category if admin" do
-    auth_token = sign_in_as(@user)
     old_category = @category.name
 
-    patch category_url(@category), params: { category: { name: "Matematika" } }, headers: {HTTP_AUTHORIZATION: "JWT #{auth_token}"}
+    patch category_url(@category), params: { category: { name: "Matematika" } }, headers: {HTTP_AUTHORIZATION: "JWT #{@admin_token}"}
     assert_not_same(true, old_category != @category.name)
   end
 
   test "should not update category if not admin" do
-    auth_token = sign_in_as(@user2)
     old_category = @category.name
 
-    patch category_url(@category), params: { category: { name: @category.name } }, headers: {HTTP_AUTHORIZATION: "JWT #{auth_token}"}
+    patch category_url(@category), params: { category: { name: @category.name } }, headers: {HTTP_AUTHORIZATION: "JWT #{@user_token}"}
 
     assert_same(true, old_category == @category.name )
   end
 
   test "should delete category if admin" do
-    auth_token = sign_in_as(@user)
-
     assert_difference("Category.count", -1) do
-      delete category_url(@category),params: {}, headers: {HTTP_AUTHORIZATION: "JWT #{auth_token}"}
+      delete category_url(@category),params: {}, headers: {HTTP_AUTHORIZATION: "JWT #{@admin_token}"}
     end
 
     assert_response :no_content
   end
 
   test "should not delete category if not admin" do
-    auth_token = sign_in_as(@user2)
-    
     assert_no_difference("Category.count") do
-      delete category_url(@category),params: {}, headers: {HTTP_AUTHORIZATION: "JWT #{auth_token}"}
+      delete category_url(@category),params: {}, headers: {HTTP_AUTHORIZATION: "JWT #{@user_token}"}
     end
 
     assert(Category.all.include?(@category))
