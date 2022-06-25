c9cc0cb4e51b8f91e4514dbb27cee42f9eeb4420
Gifar Al Anshar <gifar.anshar12@gmail.com>
2022-06-25 13:48:16 +0700

    add validation for category, category should be valid

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
     # has_many :book_categories
     # has_many :bukus, through: :book_categories
+
+    
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @category = categories(:category1)
+  end
+
+  test "category should be valid" do
+    assert @category.valid?
+  end
 end
