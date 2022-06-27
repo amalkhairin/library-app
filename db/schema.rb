bc0640ca87a90f5f7747a3d6a0f43ad62ec91cd2
imadeekosatria <imadeekosatriawiguna@gmail.com>
2022-06-27 21:15:07 +0800

    fix id for create user and save profile pict

--- db/schema.rb
+++ db/schema.rb
@@ -39,6 +39,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_24_042936) do
   end
 
   create_table "peminjaman_bukus", force: :cascade do |t|
+    t.integer "id_user"
     t.integer "id_buku"
     t.datetime "jadwal_pinjam"
     t.datetime "jadwal_kembali"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-roles = Role.create([{role: "admin"}, {role: "visitor"}])
 
-admin = User.create(name: "admin", role_id:1, username: "admin", password: "admin", email: "admin@example.com", telephone: "0812345678210")
+# You can add more role, by default we add two roles. Here are the example of the role
+roles = Role.create([{id: 1, role: "admin"}, {id: 2, role: "visitor"}])
 
-buku = Buku.create(judul:"Buku Bahasa Indonesia SMP Kelas 8", category_id:3, deskripsi: "Buku Bahasa Indonesia SMP Kelas 8",penulis:"I Made Eko Satria Wiguna",penerbit:"Gajah Pintar",jumlah_buku:12, isAvailable:1)
+admin = User.create([{name: "admin", role_id:1, username: "admin", password: "admin", email: "admin@example.com", telephone: "0812345678210"}])
 
-category = Category.create(name: "Bahasa Indonesia")
+buku = Buku.create([{judul:"Buku Bahasa Indonesia SMP Kelas 8", category_id:1, deskripsi: "Buku Bahasa Indonesia SMP Kelas 8",penulis:"I Made Eko Satria Wiguna",penerbit:"Gajah Pintar",jumlah_buku:12, isAvailable:1}])
+
+category = Category.create([{name: "Bahasa Indonesia"}])
+
+review_bukus = ReviewBuku.create([{user_id: 1, buku_id:1, rating: 4, ulasan:"Buku ini menarik sekali"}])
+
+# Role.delete_all
+# User.delete_all
+# Buku.delete_all
+# Category.delete_all
+# ReviewBuku.delete_all
