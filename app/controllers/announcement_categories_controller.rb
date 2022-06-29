49810ffbfb104efcf09e3f7bff49c7574c91ecf5
Gifar Al Anshar <gifar.anshar12@gmail.com>
2022-06-29 20:49:03 +0700

    add test for announcement_category controller, shoul create announcement categiry if admin

--- app/controllers/announcement_categories_controller.rb
+++ app/controllers/announcement_categories_controller.rb
@@ -1,10 +1,32 @@
 class AnnouncementCategoriesController < ApplicationController
   before_action :authenticate_request, except: %i[ index ]
+  before_action :category_params, only: %i[ create ]
+  before_action :require_admin, only: %i[ create ]
 
 
   def index 
     @announcement_categories = AnnouncementCategory.all
 
     render json: {status: "200", announcement_category: @announcement_categories}
+  end
+
+  def create 
+    @new_category = AnnouncementCategory.new(category_params)
+
+    if @new_category.save
+      render json: { status: "200", announcement_category: @new_category.as_json }
+    else
+      render json: { status: :unprocessable_entity, errors: @new_category.errors }
+    end
+  end
+
+  private
+
+  def category_params
+    params.permit(:category_name)
+  end
+
+  def require_admin
+    render json: {status: "200", message: "only admin can do this"} if @current_user.role.role != "admin"
   end
 end
--- test/controllers/announcement_categories_controller_test.rb
+++ test/controllers/announcement_categories_controller_test.rb
@@ -3,10 +3,30 @@ require "test_helper"
 class AnnouncementCategoriesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @announcement_category = announcement_categories(:category1)
+
+    @role1 = Role.create(role: 'admin')
+    @role2 = Role.create(role: 'visitor')
+
+    @user = User.create(name: 'Gifar', email: 'halo@example.com', username: 'gifaraja', telephone: '0812345678910',
+                        password: 'admin1', role_id: 1)
+    @user2 = User.create(name: 'Gifar Kedua', email: 'halo2@example.com', username: 'gifaraja2',
+                         telephone: '0812345678911', password: 'admin1', role_id: 2)
+
+    @admin_token = sign_in_as(@user)
+    @user_token = sign_in_as(@user2)
   end
 
   test "should get all categories" do
     get announcement_categories_path, as: :json
     assert_response :success
   end 
+
+  test 'should create announcement category if admin' do 
+    assert_difference('AnnouncementCategory.count', 1) do
+      post announcement_categories_path,
+           params: { category_name: 'Libur Lagi' } , headers: { HTTP_AUTHORIZATION: "JWT #{@admin_token}" }
+    end
+
+    assert_response :success
+  end
 end
