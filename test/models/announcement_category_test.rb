c5afe7245cbf258b30a39dd3c0c5328a9cff65ae
Gifar Al Anshar <gifar.anshar12@gmail.com>
2022-06-29 16:19:05 +0700

    add validation for announcement category model, announcement category should be valid

--- test/models/announcement_category_test.rb
+++ test/models/announcement_category_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class AnnouncementCategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do 
+    @announcement_category = announcement_categories(:category1)
+  end
+
+  test 'announcement category should be valid' do 
+    assert @announcement_category.valid?
+  end
 end
