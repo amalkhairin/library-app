76592dc576542149f2a10ab5187a4de847ce0409
imadeekosatria <imadeekosatriawiguna@gmail.com>
2022-07-01 10:55:48 +0800

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,33 +6,34 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# roles = Role.create([{id: 1, role: "admin"}, {id: 2, role: "visitor"}])
+# Data Starter. You can change the data below
+# Remove id if you got trouble with database errors
 
-# admin = User.create([{name: "admin", role_id:1, username: "admin", password: "admin", email: "admin@example.com", telephone: "0812345678210"}])
+roles = Role.create([{id: 1, role: "admin"}, {id: 2, role: "visitor"}])
 
-# buku = Buku.create([{judul:"Buku Bahasa Indonesia SMP Kelas 8", deskripsi: "Buku Bahasa Indonesia SMP Kelas 8",penulis:"I Made Eko Satria Wiguna",penerbit:"Gajah Pintar",jumlah_buku:12, is_available: true}])
+admin = User.create([{name: "admin", role_id:1, username: "admin", password: "admin", email: "admin@example.com", telephone: "0812345678210"}])
+
+user = User.create(name: 'user', role_id: 2, username: 'user', password: 'password', email: 'user@example.com', telephone: '08123444441342')
+
+# Remove id if you got trouble with database errors
+category = Category.create([{id: 1, name: 'Bahasa Indonesia' }, {id:2, name: 'IPA' }, {id:3, name: 'Ekonomi' }])
+
+buku = Buku.create([{judul:"Buku Bahasa Indonesia SMP Kelas 8", deskripsi: "Buku Bahasa Indonesia SMP Kelas 8",penulis:"I Made Eko Satria Wiguna",penerbit:"Gajah Pintar",jumlah_buku:12, is_available: true, barcode:"10294",isbn: "102ko2",category_ids:[1,2]}])
+
+review_bukus = ReviewBuku.create([{user_id: 1, buku_id:1, rating: 4, ulasan:"Buku ini menarik sekali"}])
+
+
+# Buku.create(judul:"Bahasa Indonesia SMP Kelas 8", deskripsi:"Buku Bahasa Indonesia Kelas 8",penerbit:"Gajah Dud
+#     uk",penulis:"I Made Eko",jumlah_buku:12,is_available: true, category_ids:[1,2])
 
 # category = Category.create([{name: "Bahasa Indonesia"}])
 
-# review_bukus = ReviewBuku.create([{user_id: 1, buku_id:1, rating: 4, ulasan:"Buku ini menarik sekali"}])
-
-# user = User.create(name: 'user', role_id: 2, username: 'user', password: 'password', email: 'user@example.com', telephone: '08123444441342')
-    
-# buku = Buku.create(judul:"Buku Bahasa Indonesia SMP Kelas 8", deskripsi: "Buku Bahasa Indonesia SMP Kelas 8",penulis:"I Made Eko Satria Wiguna",penerbit:"Gajah Pintar",jumlah_buku:12, is_available: true)
-    
-# category = Category.create([{ name: 'Bahasa Indonesia' }, { name: 'IPA' }, { name: 'Ekonomi' }])
-    
-# book1 = Buku.create(judul: 'Sejarah Bahasa', deskripsi: 'Mempelajari sejarah bahasa indonesia', penerbit: 'Generasi Gigih', barcode: '141234235', isbn: '12414124', penulis: 'Gifar', jumlah_buku: 3, is_available: true)
-        
-# book2 = Buku.create(judul: 'Sejarah Bahasa 2', deskripsi: 'Mempelajari sejarah bahasa indonesia 2', penerbit: 'Generasi Gigih', barcode: '1412342352', isbn: '124141242', penulis: 'Gifar', jumlah_buku: 2, is_available: true)
-            
-# book3 = Buku.create(judul: 'Sejarah Bahasa 3', deskripsi: 'Mempelajari sejarah bahasa indonesia 3', penerbit: 'Generasi Gigih', barcode: '14123342352', isbn: '1241413242', penulis: 'Gifar', jumlah_buku: 2, is_available: false)
-  
 # To delete all data from the specified table
 # Remove "#" from the beginning of the string to
 
-Role.delete_all
-User.delete_all
-Buku.delete_all
-Category.delete_all
-ReviewBuku.delete_all
+# Role.delete_all
+# User.delete_all
+# Buku.delete_all
+# Category.delete_all
+# ReviewBuku.delete_all
+# BookCategory.delete_all
